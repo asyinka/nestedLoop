772af0a50bbfcffa628a229950002cf4413924f9
asyinka <asyinka@gmail.com>
2022-07-02 18:28:01 +0100

    project placing a for loop inside another for loop to form row and column

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # if_statements
 Simple projects on if, else if, else statements and control flow
 Links JavaScript page to an HTML page which takes users input
-CONST were used to store variables
+CONST were used to store variables# nestedLoop
--- index.js
+++ index.js
@@ -1,47 +1,60 @@
-let age;
-age = 14;
-age = Number(age);
-document.getElementById("submitButton").onclick = function(){
-    age = document.getElementById("ageInput").value;
-    if(age >= 65){
-        alert("You are a senior!");
-    }
-    else if(age >= 18) {
-        alert("You are an adult!");
-    }
-    else if (age < 0) {
-        alert("You are still raining in the heavens!");
-    }
-    else {
-        alert("You are a child!");
-    }  
-}
-
-document.getElementById("submitButton2").onclick = function(){
-    const myCheckBox = document.getElementById("subscribeBox");
-    const visaPayment = document.getElementById("visaInput");
-    const mastercardPayment = document.getElementById("mastercardInput");
-    const paypalPayment = document.getElementById("paypalInput");
-
-    if (myCheckBox.checked) {
-        alert("Kindly proceed to make payment to complete subscription process");
-    }
-    else{
-        alert("Kindly subscribe");
-    }
-    if (visaPayment.checked) {
-        alert("You have chosen to pay with Visa Card");
+let symbol = window.prompt("enter a symbol");
+let row = prompt("Enter no rows")
+row = Number(row);
+let column = prompt("Enter no of columns")
+column = Number(column);
+for (let i = 0; i <= row; i++) {
+    for (let j = 0; j <= column; j++) {
+        document.getElementById("draw").innerHTML += symbol;
     } 
-    else if (mastercardPayment.checked) {
-        alert("You have chosen to pay with Master Card")
-    }
-    else if (paypalPayment.checked) {
-        alert("You are paying with Paypal")
-    }
-    else {
-        alert("Please select a payment type")
-    }
+    document.getElementById("draw").innerHTML += "<br>";
+    
 }
+
+
+
+// let age;
+// age = 14;
+// age = Number(age);
+// document.getElementById("submitButton").onclick = function(){
+//     age = document.getElementById("ageInput").value;
+//     if(age >= 65){
+//         alert("You are a senior!");
+//     }
+//     else if(age >= 18) {
+//         alert("You are an adult!");
+//     }
+//     else if (age < 0) {
+//         alert("You are still raining in the heavens!");
+//     }
+//         alert("You are a child!");
+//     }  
+
+// document.getElementById("submitButton2").onclick = function(){
+//     const myCheckBox = document.getElementById("subscribeBox");
+//     const visaPayment = document.getElementById("visaInput");
+//     const mastercardPayment = document.getElementById("mastercardInput");
+//     const paypalPayment = document.getElementById("paypalInput");
+
+//     if (myCheckBox.checked) {
+//         alert("Kindly proceed to make payment to complete subscription process");
+//     }
+//     else{
+//         alert("Kindly subscribe");
+//     }
+//     if (visaPayment.checked) {
+//         alert("You have chosen to pay with Visa Card");
+//     } 
+//     else if (mastercardPayment.checked) {
+//         alert("You have chosen to pay with Master Card")
+//     }
+//     else if (paypalPayment.checked) {
+//         alert("You are paying with Paypal")
+//     }
+//     else {
+//         alert("Please select a payment type")
+//     }
+// }
 
 
 
